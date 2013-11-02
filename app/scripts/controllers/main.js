016c8b21019cf1e40eb0167ad0dc4458ea231dbb
Anthony Gilardi <admin@appmite.com>
2013-11-02 19:11:09 -0400

    angular-base: Install base angular app using yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lifemiteAngularPhonegapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\platforms\android\AndroidManifest.xml \
