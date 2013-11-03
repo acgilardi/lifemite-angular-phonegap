dcf5fdeff6d82aa78e39b473f44fd503c5146e0f
Anthony Gilardi <admin@appmite.com>
2013-11-02 20:23:43 -0400

    restructure: Separate phonegap from angular code

---
+++ phonegap/platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes.dex : \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\BuildConfig.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\HelloWorld.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\R$attr.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\R$drawable.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\R$string.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\R$xml.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\classes\com\appmite\lifemite\R.class \
+C:\Users\Anthony\Dev\lifemite-angular-phonegap\phonegap\platforms\android\bin\dexedLibs\cordova-3.1.0-5c31ca538528c709c80f112b4226903d.jar \
